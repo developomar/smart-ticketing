4434fa9584a6e351c0b75aad6b45dfd23273ab8b
developomar <tasfiyajan2009@gmail.com>
2024-02-17 22:25:55 +0600

    creat navbar menu with responsive

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// responsive menu scripts
+function responsiveButtonOpen () {
+    const responsiveMenu = document.getElementById('responsive-menu')
+    responsiveMenu.classList.remove('hidden')
+}
+
+function responsiveButtonClose (){
+    const responsiveMenu = document.getElementById('responsive-menu')
+    responsiveMenu.classList.add('hidden')
+}
